de332cdb7b3927aec61a7b4dc108059b89147461
Marshall Warners <m@marshallwarners.com>
2026-06-16 15:31:32 -0400

    Rename session format consistency

--- plugin/rename-session.ts
+++ plugin/rename-session.ts
@@ -27,8 +27,8 @@ export default (async ({ client }) => {
                     })
 
                     return `Renamed session to ${title}`
-                }
-            })
-        }
+                },
+            }),
+        },
     }
 }) satisfies Plugin
